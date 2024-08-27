e868a93195161385cd5787ea79126555dc85be9f
Olivier Bourgeois <bourgeoisolivier@hotmail.fr>
2024-08-27 19:59:02 +0200

    navbar challenge

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,9 @@
 import { Inter } from 'next/font/google';
 import './globals.css';
 
+// alias
+import Navbar from '@/components/NavBar';
+
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
@@ -12,7 +15,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang='en'>
       <body className={inter.className}>
-        <nav className='text-2xl text-primary'>this is navbar</nav>
+        {/* <nav className='text-2xl text-primary'>this is navbar</nav> */}
+        <Navbar />
         {children}
       </body>
     </html>
