7d29d1c3f757a387a0ea485ea33dd6492f11c1ec
Olivier Bourgeois <bourgeoisolivier@hotmail.fr>
2024-08-28 07:43:32 +0200

    single drink - challenge

--- app/drinks/[id]/page.js
+++ app/drinks/[id]/page.js
@@ -1,11 +1,26 @@
-const page = ({ params }) => {
-  console.log(params);
+import Link from 'next/link';
+const url = 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=';
 
+const getSingleDrink = async (id) => {
+  const res = await fetch(`${url}${id}`);
+
+  if (!res.ok) {
+    throw new Error('Failed to fetch drink...');
+  }
+  return res.json();
+};
+
+const SingleDrink = async ({ params }) => {
+  const data = await getSingleDrink(params.id);
+  const title = data?.drinks[0]?.strDrink;
+  const imgSrc = data?.drinks[0]?.strDrinkThumb;
   return (
     <div>
-      <h1 className='text-7xl'>{params.id}</h1>
+      <Link href='/drinks' className='btn btn-primary mt-8 mb-12'>
+        back to drinks
+      </Link>
+      <h1 className='text-4xl mb-8'>{title}</h1>
     </div>
   );
 };
-
-export default page;
+export default SingleDrink;
