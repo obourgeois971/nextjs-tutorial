d3fc1255709169affbc99636c4d5ca39593b9e1a
Olivier Bourgeois <bourgeoisolivier@hotmail.fr>
2024-08-28 07:02:56 +0200

    loading component

--- README.md
+++ README.md
@@ -365,7 +365,7 @@ const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a';
 const DrinksPage = async () => {
   const response = await fetch(url);
   const data = await response.json();
-  console.log(data);
+  // console.log(data);
   return (
     <div>
       <h1 className='text-7xl'>DrinksPage</h1>
--- app/drinks/page.js
+++ app/drinks/page.js
@@ -1,9 +1,16 @@
 const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a';
 
-const DrinksPage = async () => {
+const fetchDrinks = async () => {
+  // just for demo purposes
+  await new Promise((resolve) => setTimeout(resolve, 1000));
   const response = await fetch(url);
   const data = await response.json();
-  console.log(data);
+  return data;
+};
+
+const DrinksPage = async () => {
+  const data = await fetchDrinks();
+  // console.log(data);
   return (
     <div>
       <h1 className='text-7xl'>DrinksPage</h1>
